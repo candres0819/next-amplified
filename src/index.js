a94c0d8c54defee8a189b989615a02849f0bbd4a
Carlos Cardona <carlos.cardona@pragma.com.co>
2023-07-28 10:15:17 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+import studioTheme from './ui-components/studioTheme';
+Amplify.configure(awsconfig);
+import {
+  NewForm1 
+ } from './ui-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={studioTheme}>
+      <App />
+      <NewForm1 />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
